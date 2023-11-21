871856ca1ad57598ea673d3ac788cc2836bae83f
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2023-11-21 21:25:00 +0100

    fix(cli): Validate config

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter_belgium/di/injectable.dart';
 import 'package:flutter_belgium/firebase_init.dart';
 
 Future<void> main() async {
+  const value = String.fromEnvironment('FIREBASE_PROJECT_ID');
+  print(value);
   configureDependencies();
   await initFirebase();
   runApp(const MyApp());
