07830fe86f2af72dcf0806e215e0838bd9c6c403
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2024-03-15 07:03:55 +0100

    fix: add empty cli secrets

---
+++ tools/impaktfull_cli/impaktfull_cli_secrets_empty.dart
@@ -0,0 +1,9 @@
+class ImpaktfullCliSecrets {
+  static const String onePasswordGlobalVaultName = '';
+
+  static const String opUuid1PasswordServiceAccount = '';
+
+  static const String opUuidPlaystoreCredentials = '';
+
+  static const String opUuidAppleAccount = '';
+}
