a7231223003283fdc04cf44596c29b509b957af2
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2024-04-16 19:57:45 +0200

    fix: removed the ai_barcode_scanner package

--- lib/di/injectable.config.dart
+++ lib/di/injectable.config.dart
@@ -15,16 +15,21 @@ import 'package:flutter_belgium/navigator/main_navigator.dart' as _i9;
 import 'package:flutter_belgium/repo/locale/locale_repository.dart' as _i7;
 import 'package:flutter_belgium/repo/login/login_repo.dart' as _i8;
 import 'package:flutter_belgium/repo/raffle/raffle_repo.dart' as _i11;
-import 'package:flutter_belgium/viewmodel/debug/debug_change_language_viewmodel.dart' as _i16;
-import 'package:flutter_belgium/viewmodel/debug/debug_change_target_platform_viewmodel.dart' as _i17;
+import 'package:flutter_belgium/viewmodel/debug/debug_change_language_viewmodel.dart'
+    as _i16;
+import 'package:flutter_belgium/viewmodel/debug/debug_change_target_platform_viewmodel.dart'
+    as _i17;
 import 'package:flutter_belgium/viewmodel/debug/debug_viewmodel.dart' as _i18;
 import 'package:flutter_belgium/viewmodel/global/global_viewmodel.dart' as _i6;
-import 'package:flutter_belgium/viewmodel/global/translations_viewmodel.dart' as _i15;
+import 'package:flutter_belgium/viewmodel/global/translations_viewmodel.dart'
+    as _i15;
 import 'package:flutter_belgium/viewmodel/home/home_viewmodel.dart' as _i19;
 import 'package:flutter_belgium/viewmodel/login/login_viewmodel.dart' as _i20;
-import 'package:flutter_belgium/viewmodel/qr_scanner/qr_scanner_viewmodel.dart' as _i10;
+import 'package:flutter_belgium/viewmodel/qr_scanner/qr_scanner_viewmodel.dart'
+    as _i10;
 import 'package:flutter_belgium/viewmodel/raffle/raffle_viewmodel.dart' as _i12;
-import 'package:flutter_belgium/viewmodel/raffle/raffle_winner_picker_viewmodel.dart' as _i13;
+import 'package:flutter_belgium/viewmodel/raffle/raffle_winner_picker_viewmodel.dart'
+    as _i13;
 import 'package:flutter_belgium/viewmodel/splash/splash_viewmodel.dart' as _i14;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:impaktfull_architecture/impaktfull_architecture.dart' as _i3;
@@ -43,11 +48,15 @@ extension GetItInjectableX on _i1.GetIt {
     );
     final registerModule = _$RegisterModule();
     gh.lazySingleton<_i3.Dio>(() => registerModule.provideDio());
-    gh.lazySingleton<_i4.FirebaseAuth>(() => registerModule.provideFirebaseAuth());
-    gh.lazySingleton<_i5.FirebaseFirestore>(() => registerModule.provideFirebaseFirestore());
+    gh.lazySingleton<_i4.FirebaseAuth>(
+        () => registerModule.provideFirebaseAuth());
+    gh.lazySingleton<_i5.FirebaseFirestore>(
+        () => registerModule.provideFirebaseFirestore());
     gh.lazySingleton<_i6.GlobalViewModel>(() => _i6.GlobalViewModel());
-    gh.lazySingleton<_i7.LocaleRepository>(() => _i7.LocaleRepository(gh<_i3.SharedPrefsStore>()));
-    gh.lazySingleton<_i8.LoginRepository>(() => _i8.LoginRepository(gh<_i4.FirebaseAuth>()));
+    gh.lazySingleton<_i7.LocaleRepository>(
+        () => _i7.LocaleRepository(gh<_i3.SharedPrefsStore>()));
+    gh.lazySingleton<_i8.LoginRepository>(
+        () => _i8.LoginRepository(gh<_i4.FirebaseAuth>()));
     gh.lazySingleton<_i9.MainNavigator>(() => _i9.MainNavigator());
     gh.factory<_i10.QrScannerViewModel>(() => _i10.QrScannerViewModel());
     gh.lazySingleton<_i11.RaffleRepository>(() => _i11.RaffleRepository(
@@ -59,29 +68,34 @@ extension GetItInjectableX on _i1.GetIt {
           gh<_i11.RaffleRepository>(),
           gh<_i9.MainNavigator>(),
         ));
-    gh.factory<_i13.RaffleWinnerPickerViewModel>(() => _i13.RaffleWinnerPickerViewModel(
-          gh<_i11.RaffleRepository>(),
-          gh<_i9.MainNavigator>(),
-        ));
+    gh.factory<_i13.RaffleWinnerPickerViewModel>(
+        () => _i13.RaffleWinnerPickerViewModel(
+              gh<_i11.RaffleRepository>(),
+              gh<_i9.MainNavigator>(),
+            ));
     gh.factory<_i14.SplashViewModel>(() => _i14.SplashViewModel(
           gh<_i8.LoginRepository>(),
           gh<_i9.MainNavigator>(),
         ));
-    gh.lazySingleton<_i15.TranslationsViewmodel>(() => _i15.TranslationsViewmodel(gh<_i7.LocaleRepository>()));
-    gh.factory<_i16.DebugChangeLanguageViewModel>(() => _i16.DebugChangeLanguageViewModel(
-          gh<_i15.TranslationsViewmodel>(),
-          gh<_i9.MainNavigator>(),
-        ));
-    gh.factory<_i17.DebugChangeTargetPlatformViewModel>(() => _i17.DebugChangeTargetPlatformViewModel(
-          gh<_i6.GlobalViewModel>(),
-          gh<_i9.MainNavigator>(),
-        ));
+    gh.lazySingleton<_i15.TranslationsViewmodel>(
+        () => _i15.TranslationsViewmodel(gh<_i7.LocaleRepository>()));
+    gh.factory<_i16.DebugChangeLanguageViewModel>(
+        () => _i16.DebugChangeLanguageViewModel(
+              gh<_i15.TranslationsViewmodel>(),
+              gh<_i9.MainNavigator>(),
+            ));
+    gh.factory<_i17.DebugChangeTargetPlatformViewModel>(
+        () => _i17.DebugChangeTargetPlatformViewModel(
+              gh<_i6.GlobalViewModel>(),
+              gh<_i9.MainNavigator>(),
+            ));
     gh.factory<_i18.DebugViewModel>(() => _i18.DebugViewModel(
           gh<_i6.GlobalViewModel>(),
           gh<_i15.TranslationsViewmodel>(),
           gh<_i9.MainNavigator>(),
         ));
-    gh.factory<_i19.HomeViewmodel>(() => _i19.HomeViewmodel(gh<_i9.MainNavigator>()));
+    gh.factory<_i19.HomeViewmodel>(
+        () => _i19.HomeViewmodel(gh<_i9.MainNavigator>()));
     gh.factory<_i20.LoginViewModel>(() => _i20.LoginViewModel(
           gh<_i8.LoginRepository>(),
           gh<_i9.MainNavigator>(),
--- lib/navigator/main_navigator.navigator.dart
+++ lib/navigator/main_navigator.navigator.dart
@@ -5,7 +5,6 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:flutter/material.dart' as _i2;
 import 'package:flutter/material.dart';
 import 'package:impaktfull_architecture/impaktfull_architecture.dart' as _i1;
 import 'package:impaktfull_architecture/impaktfull_architecture.dart';
@@ -14,7 +13,6 @@ import '../screen/debug/debug_change_language_screen.dart';
 import '../screen/debug/debug_change_target_platform_screen.dart';
 import '../screen/debug/debug_screen.dart';
 import '../screen/login/login_screen.dart';
-import '../screen/qr_scanner/qr_scanner_screen.dart';
 import '../screen/raffle/raffle_screen.dart';
 import '../screen/raffle/raffle_winner_picker_screen.dart';
 import '../screen/splash/splash_screen.dart';
@@ -23,19 +21,13 @@ mixin BaseNavigator {
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
   Route<dynamic>? onGenerateRoute(RouteSettings settings) {
-    final arguments = settings.arguments is Map ? (settings.arguments as Map).cast<String, dynamic>() : null;
+    final arguments = settings.arguments is Map
+        ? (settings.arguments as Map).cast<String, dynamic>()
+        : null;
     switch (settings.name) {
       case RouteNames.splashScreen:
         return MaterialPageRoute<void>(
           builder: (_) => SplashScreen(
-            key: arguments?['key'] as Key?,
-          ),
-          settings: settings,
-          fullscreenDialog: false,
-        );
-      case RouteNames.qrScannerScreen:
-        return MaterialPageRoute<void>(
-          builder: (_) => QrScannerScreen(
             key: arguments?['key'] as Key?,
           ),
           settings: settings,
@@ -93,51 +85,57 @@ mixin BaseNavigator {
     return null;
   }
 
-  void goToSplashScreen({_i1.Key? key}) => navigatorKey.currentState?.pushNamedAndRemoveUntil<dynamic>(
+  void goToSplashScreen({_i1.Key? key}) =>
+      navigatorKey.currentState?.pushNamedAndRemoveUntil<dynamic>(
         RouteNames.splashScreen,
         (_) => false,
         arguments: {'key': key},
       );
-  void goToQrScannerScreen({_i2.Key? key}) => navigatorKey.currentState?.pushNamedAndRemoveUntil<dynamic>(
-        RouteNames.qrScannerScreen,
-        (_) => false,
-        arguments: {'key': key},
-      );
-  Future<void> goToRaffleWinnerPickerScreen({_i1.Key? key}) async => navigatorKey.currentState?.pushNamed<dynamic>(
+  Future<void> goToRaffleWinnerPickerScreen({_i1.Key? key}) async =>
+      navigatorKey.currentState?.pushNamed<dynamic>(
         RouteNames.raffleWinnerPickerScreen,
         arguments: {'key': key},
       );
-  void goToRaffleScreen({_i1.Key? key}) => navigatorKey.currentState?.pushNamedAndRemoveUntil<dynamic>(
+  void goToRaffleScreen({_i1.Key? key}) =>
+      navigatorKey.currentState?.pushNamedAndRemoveUntil<dynamic>(
         RouteNames.raffleScreen,
         (_) => false,
         arguments: {'key': key},
       );
-  void goToLoginScreen({_i1.Key? key}) => navigatorKey.currentState?.pushNamedAndRemoveUntil<dynamic>(
+  void goToLoginScreen({_i1.Key? key}) =>
+      navigatorKey.currentState?.pushNamedAndRemoveUntil<dynamic>(
         RouteNames.loginScreen,
         (_) => false,
         arguments: {'key': key},
       );
-  Future<void> goToDebugChangeTargetPlatformScreen({_i1.Key? key}) async => navigatorKey.currentState?.pushNamed<dynamic>(
+  Future<void> goToDebugChangeTargetPlatformScreen({_i1.Key? key}) async =>
+      navigatorKey.currentState?.pushNamed<dynamic>(
         RouteNames.debugChangeTargetPlatformScreen,
         arguments: {'key': key},
       );
-  Future<void> goToDebugChangeLanguageScreen({_i1.Key? key}) async => navigatorKey.currentState?.pushNamed<dynamic>(
+  Future<void> goToDebugChangeLanguageScreen({_i1.Key? key}) async =>
+      navigatorKey.currentState?.pushNamed<dynamic>(
         RouteNames.debugChangeLanguageScreen,
         arguments: {'key': key},
       );
-  Future<void> goToDebugScreen({_i1.Key? key}) async => navigatorKey.currentState?.pushNamed<dynamic>(
+  Future<void> goToDebugScreen({_i1.Key? key}) async =>
+      navigatorKey.currentState?.pushNamed<dynamic>(
         RouteNames.debugScreen,
         arguments: {'key': key},
       );
   void goBack() => navigatorKey.currentState?.pop();
-  void goBackWithResult<T>({T? result}) => navigatorKey.currentState?.pop(result);
-  void popUntil(bool Function(Route<dynamic>) predicate) => navigatorKey.currentState?.popUntil(predicate);
-  void goBackTo(String routeName) => popUntil((route) => route.settings.name == routeName);
+  void goBackWithResult<T>({T? result}) =>
+      navigatorKey.currentState?.pop(result);
+  void popUntil(bool Function(Route<dynamic>) predicate) =>
+      navigatorKey.currentState?.popUntil(predicate);
+  void goBackTo(String routeName) =>
+      popUntil((route) => route.settings.name == routeName);
   Future<T?> showCustomDialog<T>({Widget? widget}) async => showDialog<T>(
         context: navigatorKey.currentContext!,
         builder: (_) => widget ?? const SizedBox.shrink(),
       );
-  Future<T?> showBottomSheet<T>({Widget? widget}) async => showModalBottomSheet<T>(
+  Future<T?> showBottomSheet<T>({Widget? widget}) async =>
+      showModalBottomSheet<T>(
         context: navigatorKey.currentContext!,
         builder: (_) => widget ?? const SizedBox.shrink(),
       );
@@ -146,15 +144,14 @@ mixin BaseNavigator {
 class RouteNames {
   static const splashScreen = '/splash';
 
-  static const qrScannerScreen = '/qr-scanner';
-
   static const raffleWinnerPickerScreen = '/raffle-winner-picker';
 
   static const raffleScreen = '/raffle';
 
   static const loginScreen = '/login';
 
-  static const debugChangeTargetPlatformScreen = '/debug-change-target-platform';
+  static const debugChangeTargetPlatformScreen =
+      '/debug-change-target-platform';
 
   static const debugChangeLanguageScreen = '/debug-change-language';
 
