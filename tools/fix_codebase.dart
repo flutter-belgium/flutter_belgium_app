0374ba5e40f7228079da5668bcfda3fd7912fa2c
Koen Van Looveren <koen.vanlooveren@impaktfull.com>
2024-05-10 14:34:27 +0200

    fix: deploy fix codebase script

--- tools/fix_codebase.dart
+++ tools/fix_codebase.dart
@@ -7,4 +7,7 @@ Future<void> main(List<String> arguments) async {
   // Fix Github credentials
   final githubCredentialsFile = File('lib/secrets/github_credentials_empty.dart');
   githubCredentialsFile.copySync('lib/secrets/github_credentials.dart');
+  // Fix impaktfull_cli secrets
+  final impaktfullCliConfigFile = File('tools/impaktfull_cli/impaktfull_cli_secrets_empty.dart');
+  impaktfullCliConfigFile.copySync('tools/impaktfull_cli/impaktfull_cli_secrets.dart');
 }
